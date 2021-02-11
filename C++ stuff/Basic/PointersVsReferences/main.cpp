2a83f03f866f38ee4f3a378d13ff065d59848537
SonaBadalyan <sona.badalyan.1992@gmail.com>
2021-02-11 22:53:19 +0400

    References are nothing but constant pointers in C++.

---
+++ C++ stuff/Basic/PointersVsReferences/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Test
+{
+    int& i;
+    int& j;
+    int& k;
+};
+
+int main()
+{
+    int i = 10;
+    int *pi = &i;
+    int &ri = i; 
+
+    ++i;
+    std::cout << i << " " << ri << " " << *pi << std::endl;
+    std::cout << &i << " " << &ri << " " << pi <<  std::endl;
+
+    ++ri;
+    std::cout << i << " " << ri << " " << *pi << std::endl;
+    std::cout << &i << " " << &ri << " " << pi <<  std::endl;
+
+    ++(*pi);
+    std::cout << i << " " << ri << " " << *pi << std::endl;
+    std::cout << &i << " " << &ri << " " << pi <<  std::endl;
+
+    ri = 20;
+    std::cout << i << " " << ri << " " << *pi << std::endl;
+    std::cout << &i << " " << &ri << " " << pi <<  std::endl;
+
+    ri = ++(*pi);
+    std::cout << i << " " << ri << " " << *pi << std::endl;
+    std::cout << &i << " " << &ri << " " << pi <<  std::endl;
+
+    *pi = 22;
+    std::cout << i << " " << ri << " " << *pi << std::endl;
+    std::cout << &i << " " << &ri << " " << pi <<  std::endl;
+    
+    std::cout << sizeof(class Test) << std::endl;
+
+    return 0;
+}
