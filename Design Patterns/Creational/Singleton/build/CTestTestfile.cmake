588a99e23d1c3de9ddfa046ae3ecf0aca22de3c2
SonaBadalyan <sona.badalyan.1992@gmail.com>
2021-02-11 16:27:47 +0400

    Delete Design Patterns/Creational/Singleton/build directory

--- Design Patterns/Creational/Singleton/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sona/Desktop/Practice/Design Patterns/Creational/Singleton
-# Build directory: /home/sona/Desktop/Practice/Design Patterns/Creational/Singleton/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
