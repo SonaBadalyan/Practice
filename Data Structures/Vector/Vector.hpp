31a41859e788ab6448336c9f13f147b5fb28175b
Sona Badalyan <sona.badalyan.1992@gmail.com>
2021-02-16 19:39:09 +0400

    Vector improvements

--- Data Structures/Vector/Vector.hpp
+++ Data Structures/Vector/Vector.hpp
@@ -8,48 +8,74 @@ class Vector
         class Iterator
         {
             public:
-                Iterator(const Vector<T> *vector, size_t iter_Intex) : m_pVector(vector), m_iter_Intex(iter_Intex){}
-                Iterator(const Iterator& rhs);
-                Iterator(const Iterator&& rhs);
+                Iterator();
+                Iterator(const Vector<T> *vector, size_t iter_Intex) 
+                                                                    :   m_pVector(vector), 
+                                                                        m_iter_Intex(iter_Intex)
+                {}
+                Iterator(const Iterator& rhs) 
+                                            :   m_pVector(rhs.m_pVector),
+                                                m_iter_Intex(rhs.m_iter_Intex)
+                {}
+                Iterator(Iterator&& rhs);
                 Iterator& operator=(const Iterator& rhs);
-                Iterator& operator=(const Iterator&& rhs);
+                Iterator& operator=(Iterator&& rhs);
+
+                Iterator& operator++(); 
+                Iterator operator++(int); 
+                Iterator& operator--(); 
+                Iterator operator--(int); 
+
                 const T& operator*() const;
-                Iterator& operator++(); // prefix
-                Iterator operator++(int); // postfix
-                Iterator& operator--(); // prefix
-                Iterator operator--(int); // postfix
                 bool operator!=(const Iterator& rhs) const;
                 bool operator==(const Iterator& rhs) const;
                 bool operator<(const Iterator& rhs) const;
                 bool operator<=(const Iterator& rhs) const;
                 bool operator>(const Iterator& rhs) const;
                 bool operator>=(const Iterator& rhs) const;
+                
             private:
                 const Vector<T> *m_pVector;
                 size_t m_iter_Intex = -1;
         };
 
     public: 
+
         Vector();
-        // Vector(const Vector& rhs);
-        // Vector(const Vector&& rhs);
-        // Vector& operator=(const Vector& rhs);
-        // Vector& operator=(const Vector&& rhs);
+        Vector(T fill_elems, size_t size);
+        Vector(size_t size);
+        
+        Vector(const Vector<T>& rhs); 
+        Vector(Vector<T>&& rhs) noexcept;
+
+        Vector<T>& operator=(const Vector<T>& rhs);
+        Vector<T>& operator=(Vector<T>&& rhs) noexcept;
+        
         void push_back(T elem);
         void insert(T elem, size_t index);
-        T get(size_t index) const;
         void pop();
+        
+        const T& at(size_t index) const;
+        const T& back() const;
+        const T& front() const;
+        const T& operator[](size_t index) const;
+
         size_t size() const;
+        bool empty() const;
         size_t get_capacity() const;
+
         void print() const;
-        void reserve(size_t how_many_times);
-        const T& operator[](size_t index);
+
+        void reserve(size_t capacity);
+
         Iterator begin() const;
         Iterator end() const;
 
+        ~Vector();
+
     private:
-        T *m_array = nullptr;
-        size_t m_index = 0;
+        T *buffer = nullptr;
+        size_t m_index = -1;
         size_t m_capacity = 0;
 };
 
--- Data Structures/Vector/main.cpp
+++ Data Structures/Vector/main.cpp
@@ -9,6 +9,12 @@ int main()
     v.push_back(30);
     v.push_back(40);
     v.push_back(50);
+
+    // Vector<int> v1(std::move(v));
+    // Vector<int> v2;
+    // v2 = v1;
+    // Vector<int> v3;
+    // v3 = std::move(v2);
  
     std::cout << "Vector size : " << v.size() << std::endl;
     std::cout << "Vector capacity : " << v.get_capacity() <<  std::endl;
@@ -18,7 +24,7 @@ int main()
  
     v.insert(60, 3);
 
-    std::cout << "\nAfter updating 1st index" <<  std::endl;
+    std::cout << "\nAfter updating 4th index" <<  std::endl;
     v.print();
 
     v.pop();
@@ -28,10 +34,19 @@ int main()
     std::cout << "Vector size of type int: " << v.size() <<  std::endl;
     std::cout << "Vector capacity of type int : " << v.get_capacity() <<  std::endl;
 
-    std::cout << "Element at 1st index of type int: " << v.get(0) <<  std::endl;
+    // std::cout << "Element at 1st index of type int: " << v3.at(3) <<  std::endl;
 
     std::cout << "Vector elements of type int: ";
     v.print();
+    std::cout << "v.size(): " << v.size() << std::endl;
+    v.pop();
+    std::cout << "v.size(): " << v.size() << std::endl;
+    std::cout << "v.at(2): " << v.at(2) << std::endl;
+    std::cout << "v[2]: " << v[2] << std::endl;
+    std::cout << "v.empty() " << v.empty() << std::endl;
+    std::cout << "v.front(): " << v.front() << std::endl;
+    std::cout << "v.back(): " << v.back() << std::endl;
+
 
     return 0;
 }
