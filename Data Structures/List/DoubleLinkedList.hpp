7241d4b35edaaed29b618249963dfbdcfaf66e29
Sona Badalyan <sona.badalyan.1992@gmail.com>
2021-02-20 03:12:01 +0400

    added double linked list incomplete implementation

--- Data Structures/List/DoubleLinkedList.hpp
+++ Data Structures/List/DoubleLinkedList.hpp
@@ -2,87 +2,379 @@
 #define DOUBLE_LINKED_LIST_H
 
 #include <cstddef>
+#include <iostream>
 
 template <typename T>
 class DoubleLinkedList
 {
     public:
-        struct Node
-        {
-            T data;
-            Node *prev;
-            Node *next;
-        };
-
-        class Iterator
-        {
-
-        };
 
     typedef T value_type;
     typedef size_t size_type;
+    typedef T* pointer;
     typedef value_type& reference;
     typedef const value_type& const_reference;
+
+    struct Node
+    {
+        value_type data = value_type();
+        Node *prev = nullptr;
+        Node *next = nullptr;
+    };
+
+    class Iterator
+    {
+        public:
+            Iterator() = delete;
+            Iterator(value_type elem);
+            Iterator(const Iterator& rhs);
+            Iterator& operator=(const Iterator& rhs);
+
+            Iterator& operator++(); 
+            Iterator operator++(int); 
+            Iterator& operator--(); 
+            Iterator operator--(int); 
+
+            const T& operator*() const;
+
+            bool operator==(const Iterator& rhs) const;
+            bool operator!=(const Iterator& rhs) const;
+            bool operator<(const Iterator& rhs) const;
+            bool operator<=(const Iterator& rhs) const;
+            bool operator>(const Iterator& rhs) const;
+            bool operator>=(const Iterator& rhs) const;
+                            
+        private:
+
+            pointer m_elem = nullptr;
+    };
+
     typedef Iterator iterator;
     typedef const Iterator const_iterator;
     
-    DoubleLinkedList();
-    explicit DoubleLinkedList( size_type count );
-    DoubleLinkedList(size_t size, T elem);
-    DoubleLinkedList(const DoubleLinkedList<T>& rhs);
-    DoubleLinkedList(DoubleLinkedList<T>&& rhs);
+    DoubleLinkedList() : head (nullptr), tail (nullptr) {}
+    
+    explicit DoubleLinkedList( size_type count, const value_type& val = value_type() )
+    {
+        if ( 1 == count)
+        { 
+            head = new Node();
+            head->data = val;
+            tail = head;
+            return;
+        }
 
-    DoubleLinkedList& operator=(const DoubleLinkedList<T>& rhs);
-    DoubleLinkedList& operator=(DoubleLinkedList<T>&& rhs);
+        if ( 2 == count )
+        {
+            head = new Node();
+            tail = new Node();
 
-    iterator begin();
-    const_iterator begin() const;
-    const_iterator cbegin() const;
+            head->next = tail;
+            head->data = val;
 
-    iterator end();
-    const_iterator end() const;
-    const_iterator cend() const;
+            tail->prev = head;
+            tail->data = val;
 
-    // reverse_iterator rbegin();
-    // reverse_iterator rbegin() noexcept;
-    // const_reverse_iterator rbegin() const;
-    // const_reverse_iterator rbegin() const noexcept;
-    // const_reverse_iterator crbegin() const noexcept;
+            return;
+        }
 
-    // reverse_iterator rend();
-    // reverse_iterator rend() noexcept;
-    // const_reverse_iterator rend() const;
-    // const_reverse_iterator rend() const noexcept;
-    // const_reverse_iterator crend() const noexcept;
+        if ( 3 == count )
+        {
+            head = new Node();
+            Node *mid = new Node();
+            tail = new Node();
 
-    reference front();
-    const_reference front() const;
-    reference back();
-    const_reference back() const;
+            head->next = mid;
+            head->data = val;
+            
+            mid->prev = head;
+            mid->next = tail;
+            mid->data = val;
 
-    bool empty() const;
-    size_t size() const;
+            tail->prev = mid;
+            tail->data = val;
 
-    void clear() noexcept;
-    iterator insert( iterator pos, const_reference value );
-    iterator erase( iterator pos );
-    iterator erase( iterator first, iterator last );
-    void push_back( const_reference value );
-    void pop_back();
-    void push_front( const_reference value );
-    void pop_front();
-    void resize( size_type count );
-    void resize( size_type count, T value = T() );
-    void resize( size_type count, const value_type& value );
-    void swap( DoubleLinkedList& other );
+            return;
+        }
 
-    ~DoubleLinkedList();
+        head = new Node();    
+        Node *curr = new Node();
+        Node *next = new  Node();
+        tail = new Node();
+
+        head->next = curr;
+        head->data = val;
+
+        curr->prev = head;
+        curr->next = next; 
+        curr->data = val;
+
+        next->prev = curr;
+        next->data = val;
+
+        size_type remain = count - 4;
+
+        while ( remain-- )
+        {
+            curr = next;
+            next = new Node();
+            curr->next = next;
+            curr->data = val;
+            next->prev = curr;
+        }
+
+        next->next = tail;
+        next->data = val;
+
+        tail->prev = next;
+        tail->data = val;
+    }
+
+    DoubleLinkedList(const DoubleLinkedList<value_type>& rhs) 
+    {
+        if ( rhs.head == nullptr)
+            head = tail = nullptr;
+        else
+        { 
+            Node *curr = rhs.head;
+
+            while (curr != nullptr)
+            {
+                push_back(curr->data),
+                curr = curr->next;
+            }
+        }
+    }
+
+    DoubleLinkedList(DoubleLinkedList<value_type>&& rhs)
+                                                        :   head(rhs.head),
+                                                            tail(rhs.tail)
+    {
+        rhs.head = nullptr;
+        rhs.tail = nullptr;
+    }
+
+    DoubleLinkedList<value_type>& operator=(const DoubleLinkedList<value_type>& rhs) 
+    {
+        if ( this != &rhs )
+        {
+            if ( rhs.head == nullptr )
+                head = tail = nullptr;
+            else
+            { 
+                Node *curr = rhs.head;
+
+                while (curr != nullptr)
+                {
+                    push_back(curr->data),
+                    curr = curr->next;
+                }
+            }
+        }
+        return *this;
+    }
+                                                                                    
+
+    DoubleLinkedList<value_type>& operator=(DoubleLinkedList<value_type>&& rhs)
+    {
+        if ( this != &rhs )
+        {
+            head = rhs.head;
+            tail = rhs.tail;
+            rhs.head = nullptr;
+            rhs.tail = nullptr;
+        }
+        return *this;
+    }
+
+    // iterator begin()
+    // {
+    //     return iterator(head);
+    // }
+    // const_iterator begin() const;
+    // const_iterator cbegin() const;
+
+    // iterator end();
+    // const_iterator end() const;
+    // const_iterator cend() const;
+
+    reference front() 
+    {
+        return head->data;
+    }
+
+    const_reference front() const 
+    {
+
+    }
+
+    reference back()
+    {
+        return tail->data;
+    }
+
+    const_reference back() const
+    {
+
+    }
+
+    bool empty() const
+    {
+        return head == nullptr;
+    }
+
+    size_t size() const
+    {
+        Node *curr = head;
+        size_t size = 0;
+
+        while (curr != nullptr)
+        {
+            ++size;
+            curr = curr->next;
+        }
+
+        return size;
+    }
+
+    void clear() noexcept
+    {
+
+    }
+
+    iterator insert( iterator pos, const_reference value )
+    {
+
+    }
+
+    iterator erase( iterator pos )
+    {
+
+    }
+
+    iterator erase( iterator first, iterator last )
+    {
+
+    }
+
+    void push_back( const_reference value )
+    {
+        if ( tail == nullptr )
+        {
+            tail = new Node();
+            tail->data = value;
+            head = tail;
+        }
+        else
+        {
+            Node *new_node = new Node();
+            new_node->data = value;
+            new_node->prev = tail;
+            new_node->next = nullptr;
+            tail->next = new_node;
+            tail = new_node;
+        }
+    }
+
+    void pop_back()
+    {
+        if ( 1 == size() )
+        {
+            delete tail;
+            tail = head = nullptr;
+            return;
+        }
+
+        if ( !empty() )
+        {
+            Node *new_tail = tail;
+            new_tail = tail;
+
+            tail = tail->prev;
+            tail->next = nullptr;
+
+            delete new_tail;
+        }
+    }
+
+    void push_front( const_reference value )
+    {
+        if ( head == nullptr )
+        {
+            head = new Node();
+            head->data = value;
+            tail = head;
+        }
+        else
+        {
+            Node *new_node = new Node();
+            new_node->prev = nullptr;
+            new_node->data = value;
+            new_node->next = head;
+            head = new_node;
+        }
+    }
+    
+    void pop_front()
+    {
+        if ( 1 == size() )
+        {
+            delete head;
+            head = tail = nullptr;
+            return;
+        }
+
+        if ( !empty() )
+        {
+            Node *new_head = head;
+            new_head = head;
+
+            head = head->next;
+            head->prev = nullptr;
+
+            delete new_head;
+        }
+    }
+
+    void resize( size_type count, value_type value = value_type() )
+    {
+
+    }
+
+    void swap( DoubleLinkedList<value_type>& other )
+    {
+
+    }
+
+    void print () const
+    {
+        Node *curr = head;
+
+        while (curr != nullptr)
+        {
+            std::cout << curr->data << " ";
+            curr = curr->next;
+        }
+
+        std::cout << std::endl;
+
+    }
+
+    ~DoubleLinkedList()
+    {
+        Node *curr = head;
+
+        while ( curr != nullptr )
+        {
+            Node* next = curr->next;
+            delete curr;
+            curr = next;
+        }
+    }
 
     private:
-        size_type m_index = 0;
         Node *head = nullptr;
         Node *tail = nullptr;
-
 };
 
 template< typename T >
--- Data Structures/List/main.cpp
+++ Data Structures/List/main.cpp
@@ -1,6 +1,37 @@
 #include "DoubleLinkedList.hpp"
+#include  <string>
+// #include <list>
 
 int main()
 {
+    // std::list<int> l(10, 4);
+    // std::list<int> l1(10, 3);
+
+    DoubleLinkedList<int> list;
+    DoubleLinkedList<int> list1(10, 3);
+    DoubleLinkedList<int> list2(list1);
+    DoubleLinkedList<int> list3(std::move(list2));
+    list = list1;
+    list2 = std::move(list);
+
+    list1.print();
+
+    std::string result;
+    if (list1.empty())
+        result = "empty";
+    else
+        result = "non empty";
+
+    std::cout << result <<  std::endl;
+
+    list1.push_front(9);
+    list1.push_back(10);
+    list1.print();
+    list1.pop_front();
+    list1.print();
+    list1.pop_back();
+    list1.print();
+
+
     return 0;
 }
