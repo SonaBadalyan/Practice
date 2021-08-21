b21ad35a2a5b6cc77836821bcccc447d0f04f67b
SonaBadalyan <sona.badalyan.1992@gmail.com>
2021-08-21 13:16:34 +0400

    2D array and pointers

--- Data Structures/Arrays/2DArrays/main.cpp
+++ Data Structures/Arrays/2DArrays/main.cpp
@@ -9,16 +9,18 @@ int main()
     // Address of a[i][j] = Base address + ((i * n) + j) * sizeof(a[i][j])
 
     // Column major implementation 
-    // Address of a[i][j] = Base address + ((j * m) + i) * sizeof(a[i][j])
+    // Address of a[i][j] = Base address + ((j * m) + i) * sizeof(a[i][j]),  m is a number of rows
 
     for (int i = 0; i < 2; ++i)
     {
-        for (int j = 0; j <3; ++ j)
+        for (int j = 0; j < 3; ++j)
         {
-            std::cout << "Enter element: ";
-            std::cin >> array[i][j];
+            // std::cout << "Enter element: ";
+            // std::cin >> array[i][j];
 
-            std::cout << std::endl;
+            // std::cout << std::endl;
+
+            array[i][j] = ((i + 1) + (j + 1)) * 10;
         }
     }
 
@@ -30,6 +32,40 @@ int main()
         }
     }
 
+    std::cout << std::endl;
+    std::cout << "sizeof(array[0][0]) : " << sizeof(array[0][0]) << std::endl;
+
+    int* parr = &array[0][0];
+
+    std::cout << "array : " << array << std::endl;
+    std::cout << "parr : " << parr << std::endl;
+
+    parr = array[0];
+
+    std::cout << "parr : " << parr << std::endl;
+
+    std::cout << "&array[0] : " << &array[0] << std::endl;
+    std::cout << "&array[0][0] : " << &array[0][0] << std::endl;
+    std::cout << "&array : " << &array << std::endl;
+    std::cout << "*array : " << *array << std::endl;
+    std::cout << "array[0] : " << array[0] << std::endl;
+
+    std::cout << " array + 1 : " << (array + 1) << std::endl;
+    std::cout << " *(array + 1) : " << *(array + 1 ) << std::endl;
+    std::cout << "&array[1] : " << &array[1] << std::endl;
+    std::cout << "array[1] : " << array[1] << std::endl;
+    std::cout << "&array[1][0] : " << &array[1][0] << std::endl;
+    
+    std::cout << "*(array + 1) + 2 : " << *(array + 1) + 2 << std::endl;
+    std::cout << "*(*(array + 1) + 2) : " << *(*(array + 1) + 2) << std::endl;
+    std::cout << "array[1][2] : " << array[1][2] << std::endl;
+    std::cout << "*(array[1] + 2) : " << *(array[1] + 2) << std::endl;
+    // array[i][j] = *(*(array + i) + j) = * (array[i] + j) = parr[i][j] = *(*(parr + i) + j) = * (parr[i] + j)
+    std::cout << "*(*array + 1) : " << *(*array + 1)  << std::endl;
+    std::cout << "**array : "  << **array << std::endl;
+    //std::cout << "**parr : "  << **parr << std::endl;
+    std::cout << "array[1] + 1: " << array[1] + 1 << std::endl;
+    std::cout << "&array[1] + 1: " << &array[1] + 1 << std::endl;
     std::cout << std::endl;
 
     return 0;
